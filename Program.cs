dd39af55927bbbc6ab4c86ac18dd299bb4c2abd1
Ammon Whitaker <AmmWhi8@millardk12.org>
2026-04-15 13:17:39 -0600

    Finished withdraw and deposit, started on loging process

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// Ammon Whitaker 3/25/2026 Lab 10 - ATM Machine
 
 using System.Diagnostics;
+using System.Net.NetworkInformation;
 
 const string accountsFilePath = "Accounts.txt";
 
@@ -23,7 +24,7 @@ void testProgram()
 
     FileHandling.Write(accountsFilePath, accounts);
 
-    (string, int, decimal, List<string>) readAccount = FileHandling.Read(accountsFilePath)[^1];
+    (string name, int pin, decimal balance, List<string> transactions) readAccount = FileHandling.Read(accountsFilePath)[^1];
 
     Debug.Assert(readAccount.Item1 == newAccount.Item1);
     Debug.Assert(readAccount.Item2 == newAccount.Item2);
@@ -36,10 +37,19 @@ void testProgram()
     Debug.Assert(ValidatePin(51423, readAccount) == true); // correct pin
 
     // Withdraw
-    decimal currentBal = readAccount.Item3;
+    decimal startBal = readAccount.Item3;
 
-    Debug.Assert(Withdraw((decimal)0.001, readAccount) == false);
+    Debug.Assert(Withdraw((decimal)0.001, ref readAccount) == false);
+    Debug.Assert(Withdraw(startBal + 100, ref readAccount) == false);
+    Debug.Assert(Withdraw(-10,            ref readAccount) == false);
+    Debug.Assert(Withdraw((decimal)50.50, ref readAccount) == true);
+    Debug.Assert(startBal == readAccount.balance + (decimal)50.50);
 
+    // Deposit
+    Debug.Assert(Deposit((decimal)0.001, ref readAccount) == false);
+    Debug.Assert(Deposit(-50,            ref readAccount) == false);
+    Debug.Assert(Deposit((decimal)50.50, ref readAccount) == true);
+    Debug.Assert(startBal == readAccount.balance);
 
     FileHandling.Write(accountsFilePath, accounts); // Remove the debug account
 }
@@ -49,6 +59,30 @@ Console.Clear();
 // Read the account data
 List<(string name, int pin, decimal balance, List<string> transactions)> accounts = FileHandling.Read("Accounts.txt");
 
+// Login to account
+Console.WriteLine("Welcome to the Account-it program!");
+Console.WriteLine("Please Login to continue:");
+Console.WriteLine();
+
+string? accountName;
+int pin;
+int accountIndex;
+do
+{
+    do
+    {
+        Console.Write("Username: ");
+        accountName = Console.ReadLine();
+        Console.Write("Pin: ");
+        pin = Convert.ToInt32(Console.ReadLine());
+        accountIndex = accounts.FindIndex(account => account.name == accountName); // No overlapping names
+    } while (pin >= 0);
+} while (ValidatePin(pin, accounts[accountIndex]));
+
+
+// Runtime loop
+
+
 FileHandling.Write("Accounts.txt", accounts);
 
 bool ValidatePin(int pin, (string name, int pin, decimal balance, List<string> transactions) account)
@@ -56,12 +90,27 @@ bool ValidatePin(int pin, (string name, int pin, decimal balance, List<string> t
     return pin == account.pin;
 }
 
-bool Withdraw(decimal request, (string name, int pin, decimal balance, List<string> transactions) account)
+bool Withdraw(decimal request, ref (string name, int pin, decimal balance, List<string> transactions) account)
 {
     // Make sure the transaction is possible
     if (Math.Round(request, 2) != request) {return false;}; // No withdraw smaller than a penny
     if (account.balance < request) {return false;} // Can't go into debt
+    if (request < 0) {return false;} // Can't go into debt
 
     // actual transaction goes here
+    account.balance -= request;
+    account.transactions.Add($"{DateTime.Now} --- Withdrew ${request:F2}");
+    return true;
+}
+
+bool Deposit(decimal request, ref (string name, int pin, decimal balance, List<string> transactions) account)
+{
+    // Make sure the transaction is possible
+    if (Math.Round(request, 2) != request) {return false;}; // No withdraw smaller than a penny
+    if (request < 0) {return false;} // Can't deposit debt
+
+    // actual transaction goes here
+    account.balance += request;
+    account.transactions.Add($"{DateTime.Now} --- Deposited ${request:F2}");
     return true;
 }
--- obj/Debug/net8.0/lab-10-atm-machine-AnimalWhitAcre77.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-10-atm-machine-AnimalWhitAcre77.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-10-atm-machine-AnimalWhitAcre77")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4d6dbde8f12f48117cc8d88e51d1f2856607422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d63c7f00c8420a699c1e7b8cf063abac2136c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-10-atm-machine-AnimalWhitAcre77")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-10-atm-machine-AnimalWhitAcre77")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
